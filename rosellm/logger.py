4ba8a6eaacee0eaeded344e75612292bdde80aa7
wineandchord <guoqizhou19@gmail.com>
2025-01-08 22:40:41 +0800

    rosellm: update requirements.txt and logger.py format

--- rosellm/logger.py
+++ rosellm/logger.py
@@ -1,10 +1,61 @@
 import logging
 from logging import Logger
 
-_FORMAT = "%(levelname)s"
+"""
+The format of the log message.
+Example: DEBUG 01-01 12:00:00 rosellm.py:12] Hello, world!
+%(xx)s indicates outputting xx as a string.
+%(xx)d indicates outputting xx as a decimal integer.
+levelname: DEBUG, INFO, WARNING, ERROR, CRITICAL
+# asctime is ascii time.
+asctime: 01-01 12:00:00
+filename: rosellm.py
+lineno: 12
+message: Hello, world!
+"""
+_FORMAT = (f"%(levelname)s "
+           "%(asctime)s "
+           "%(filename)s:"
+           "%(lineno)d] "
+           "%(message)s")
+
+"""
+The date format is used to format the date and time 
+in the log message.
+Example: 01-01 12:00:00
+"""
+_DATE_FORMAT = "%m-%d %H:%M:%S"
+
+DEFAULT_LOGGING_CONFIG = {
+    "formatters": {
+        "rosellm": {
+            "class": "rosellm.logger.NewLineFormatter",
+            "datefmt": _DATE_FORMAT,
+            "format": _FORMAT,
+        },
+    },
+    "handlers": {
+        "rosellm": {
+            "class": "logging.StreamHandler",
+            "formatter": "rosellm",
+            "level": "DEBUG",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "rosellm": {
+            "handlers": ["rosellm"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
+
+def NewLineFormatter():
+    return logging.Formatter(_FORMAT, _DATE_FORMAT)
 
 def _config_root_logger():
-    pass
+    logging.config.dictConfig(DEFAULT_LOGGING_CONFIG)
 
 _config_root_logger()
 
